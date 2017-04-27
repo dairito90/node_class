3fdc6b39a6b176e1c7539d0485f1c1f429c4d15e
Dairon Rodriguez <drodriguez@fvi.edu>
2017-04-27 18:41:28 -0400

    fix code async

--- photoServer.js
+++ photoServer.js
@@ -10,9 +10,30 @@ function load_album_list(callback) {
                 callback(err);
                 return;
             }
-            callback(null, files);
+
+            var only_dirs = [];
+
+            var interator = (index) => {
+                if (index == files.length) {
+                    callback(null, only_dirs);
+                    return;
+                }
+
+
+            fs.stat ('albums/' + files[index],(err,stats) => {
+                if (err) {
+                    callback(err);
+                    return;
+                }
+                if (stats.isDirectory()) {
+                    only_dirs.push(files[index]);
+                }
+                interator(index + 1);
+            });
         }
-    );
+
+    interator(0);
+   });
 }
 
 
@@ -28,8 +49,15 @@ function handle_incoming_request(req, res) {
             return;
         }
 
-        var out = { error: null, data: {albums: albums}};
-        res.writeHead(200, {"Content-Type": "application/json"})
+        var out = {
+            error: null,
+            data: {
+                albums: albums
+            }
+        };
+        res.writeHead(200, {
+            "Content-Type": "application/json"
+        })
         res.end(JSON.stringify(out) + "\n");
     });
 }
