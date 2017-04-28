a08f43eea82169e22c51c55974cceb9552bc12da
Dairon Rodriguez <drodriguez@fvi.edu>
2017-04-27 20:06:24 -0400

    created express server basic

--- express2/server2.js
+++ express2/server2.js
@@ -1,13 +1,14 @@
-var express = require ('express');
+var express = require('express');
 var app = express();
-const PORT = 8084;
+const PORT = 8085;
+ 
 
 app.get('/',function(req,res) {
-    res.send('hello world');
+    res.send('Dairon create a Express Server!');
 });
 
 
 
 app.listen(PORT,function(){
     console.log('listening on port ${PORT}');
-});
+})
